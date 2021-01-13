5a606ed5829142c9fe93fbeca1bb43e38a5d50ae
蔡奕俊 <yijun.cai@jimicn.com>
2021-01-13 18:49:49 +0800

    feat: 游戏页面布局

--- src/views/drawing_pages/Game/game.vue
+++ src/views/drawing_pages/Game/game.vue
@@ -1,6 +1,11 @@
+<link rel="stylesheet" href="../../../styles/views/Game/game.scss">
 <template>
   <div class="game-page">
-    <top-bar />
+    <div class="top-bar">
+      <top-bar />
+    </div>
+    <div class="drawing-area"></div>
+    <div class="chat-area">1</div>
   </div>
 </template>
 
